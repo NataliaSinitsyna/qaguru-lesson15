d7a6453df59de36a48dd14a841b35d680260e9ef
NataliaSinitsyna <natalia.a.sinitsyna@gmail.com>
2023-10-16 22:24:03 +0300

    change classpath in AuthConfig

--- src/test/java/rtlabs/config/AuthConfig.java
+++ src/test/java/rtlabs/config/AuthConfig.java
@@ -3,8 +3,8 @@ package rtlabs.config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:src/test/resources/config/secret.properties",
-        "classpath:auth.properties"
+        "classpath:src/test/resources/config/secret.properties"
+        //"classpath:auth.properties"
 })
 
 public interface AuthConfig extends Config {
